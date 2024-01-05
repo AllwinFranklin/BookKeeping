d3332e841d3367325317f964eaa299331a9a6d44
AllwinFranklin <allwinanto360@gmail.com>
2024-01-05 15:24:52 +0530

    #2 Fixed 2 endpoint issue caused by stored procedure.
    ### Fix
    Added `PublishedDate` Column from `Book` table to GetAllBooksSortByAurthorTitleSP & GetAllBooksSortByPublisherAurthorTitleSP

---
+++ BookKeeping.API/Migrations/20240105093638_GetAllBooksSortByPublisherAurthorTitleSP1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookKeeping.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class GetAllBooksSortByPublisherAurthorTitleSP1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string procedure = "ALTER PROCEDURE [dbo].[GetAllBooksSortByPublisherAurthorTitleSP]\r\nAS\r\n\tSELECT Id, Publisher, AuthorLastName, AuthorFirstName, Title, Price, PublishedDate\r\n\tFROM Books\r\n\tORDER BY Publisher, AuthorLastName, AuthorFirstName, Title\r\nRETURN 0\r\n";
+            migrationBuilder.Sql(procedure);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            string procedure = "DROP PROCEDURE [dbo].[GetAllBooksSortByPublisherAurthorTitleSP]";
+            migrationBuilder.Sql(procedure);
+        }
+    }
+}
